e04caf83590ff7333f80f7fe5f8f9fceeb5a3dde
Thomerlin <thomazzribeiro@hotmail.com>
2021-08-30 09:05:40 -0300

    testers

--- ft_printf.c
+++ ft_printf.c
@@ -11,16 +11,17 @@ void	category_scanner(t_print *box)
 
 void	parse_formatting(t_print *box)
 {
-	void	(*category_is[8])(t_print *);
+	void	(*category_is[9])(t_print *);
 
 	category_is[0] = &category_is_char;
 	category_is[1] = &category_is_string;
 	category_is[2] = &category_is_hexa_ptr;
 	category_is[3] = &category_is_deci;
-	category_is[4] = &category_is_unsign;
-	category_is[5] = &category_is_hexa;
-	category_is[6] = &category_is_hexa_up;
-	category_is[7] = &category_is_modulo;
+	category_is[4] = &category_is_deci;
+	category_is[5] = &category_is_unsign;
+	category_is[6] = &category_is_hexa;
+	category_is[7] = &category_is_hexa_up;
+	category_is[8] = &category_is_modulo;
 	box->size = 0;
 	while (box->str[box->pos])
 	{
